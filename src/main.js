b3940aa2c1b76a31518f99a429b03cd43e52e18a
Redd500 <reddfiveohoh@gmail.com>
2016-12-30 21:49:37 -0500

    ugh

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 "use strict";
+require('./polyfills.ts');
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var core_1 = require('@angular/core');
-var app_module_1 = require('./app.module');
-core_1.enableProdMode();
+var environment_1 = require('./environments/environment');
+var app_module_1 = require('./app/app.module');
+if (environment_1.environment.production) {
+    core_1.enableProdMode();
+}
 platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
 //# sourceMappingURL=main.js.map
